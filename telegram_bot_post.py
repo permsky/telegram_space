7e4b8c9c7592f65b1d862f7ff7df482f09f79412
Vitaly Borodulin <rainbowf@mail.ru>
2021-11-11 23:33:49 +0500

    Publish all photos from directory to Telegram with delay

--- telegram_bot_post.py
+++ telegram_bot_post.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import telegram
 from dotenv import load_dotenv
@@ -17,9 +18,21 @@ def post_image(bot: telegram.Bot, chat_id: str, image: str) -> None:
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('TELEGRAM_TOKEN')
+    delay = os.getenv('POST_DELAY')
     bot = telegram.Bot(token=token)
     chat_id = '@CuriousAstro'
     message_text = 'Добро пожаловать!'
     post_message(bot, chat_id, message_text)
-    image = 'apod_images/apod0.jpg'
-    post_image(bot, chat_id, image)
+    directory = 'apod_images'
+    images_list = os.listdir(directory)
+    while True:
+        for image_number, image in enumerate(images_list):
+            image = f'{directory}/{image}'
+            post_image(bot, chat_id, image)
+            if image_number == len(images_list) - 1:
+                print('All images are published.')
+                exit(0)
+            if delay:
+                time.sleep(int(delay))
+            else:
+                time.sleep(3600 * 24)
