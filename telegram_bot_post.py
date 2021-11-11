9cced56390e27d9c5436b76914f87832eea4042d
Vitaly Borodulin <rainbowf@mail.ru>
2021-11-11 22:38:06 +0500

    Modify post_message and post_image functions

--- telegram_bot_post.py
+++ telegram_bot_post.py
@@ -4,23 +4,22 @@ import telegram
 from dotenv import load_dotenv
 
 
-def post_message(token: str, chat_id: str, text: str) -> None:
+def post_message(bot: telegram.Bot, chat_id: str, text: str) -> None:
     """Post message in Telegram channel."""
-    astro_bot = telegram.Bot(token=token)
-    astro_bot.send_message(chat_id=chat_id, text=text)
+    bot.send_message(chat_id=chat_id, text=text)
 
 
-def post_image(token: str, chat_id: str, image: str) -> None:
+def post_image(bot: telegram.Bot, chat_id: str, image: str) -> None:
     """Post photo in Telegram channel."""
-    astro_bot = telegram.Bot(token=token)
-    astro_bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
+    bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
 
 
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('TELEGRAM_TOKEN')
+    bot = telegram.Bot(token=token)
     chat_id = '@CuriousAstro'
     message_text = 'Добро пожаловать!'
-    post_message(token, chat_id, message_text)
+    post_message(bot, chat_id, message_text)
     image = 'apod_images/apod0.jpg'
-    post_image(token, chat_id, image)
+    post_image(bot, chat_id, image)
