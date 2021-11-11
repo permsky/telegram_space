f681c74b54bc4d0459b4d0a3f108d8797cfe0efa
Vitaly Borodulin <rainbowf@mail.ru>
2021-11-11 22:14:00 +0500

    Publish photo to Telegram channel

--- telegram_bot_post.py
+++ telegram_bot_post.py
@@ -10,9 +10,17 @@ def post_message(token: str, chat_id: str, text: str) -> None:
     astro_bot.send_message(chat_id=chat_id, text=text)
 
 
+def post_image(token: str, chat_id: str, image: str) -> None:
+    """Post photo in Telegram channel."""
+    astro_bot = telegram.Bot(token=token)
+    astro_bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
+
+
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('TELEGRAM_TOKEN')
     chat_id = '@CuriousAstro'
     message_text = 'Добро пожаловать!'
     post_message(token, chat_id, message_text)
+    image = 'apod_images/apod0.jpg'
+    post_image(token, chat_id, image)
