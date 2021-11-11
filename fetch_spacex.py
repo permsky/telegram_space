5c68423cb8e7f7ae6c84a83eaf6425415b099acf
Vitaly Borodulin <rainbowf@mail.ru>
2021-11-11 23:41:57 +0500

    Download photos from SpaceX launch

---
+++ fetch_spacex.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import requests
+
+
+def get_photo_links(url: str) -> list:
+    """Return list of links on photos of SpaceX launch."""
+    response = requests.get(url)
+    response.raise_for_status() 
+    return response.json()['links']['flickr_images']
+
+
+def download_image(url: str, image_name: str) -> None:
+    """Download image from url."""
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(image_name, 'wb') as img:
+        img.write(response.content)
+
+
+def fetch_spacex_launch(url: str, directory: str) -> None:
+    """Fetch SpaceX launch photos into determined directory."""
+    photo_links = get_photo_links(url)
+    Path(directory).mkdir(exist_ok=True)
+    for photo_number, photo_link in enumerate(photo_links):
+        image_name = f'{directory}spacex{photo_number}.jpg' 
+        download_image(photo_link, image_name)
+    
+
+if __name__ == '__main__':
+    spacex_launch_url = 'https://api.spacexdata.com/v3/launches/99'
+    images_directory = './images/'
+    fetch_spacex_launch(spacex_launch_url, images_directory)
