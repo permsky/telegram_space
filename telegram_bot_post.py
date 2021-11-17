a1972a43a41284f850e8b6064ed9b69047f0fb0b
Vitaly Borodulin <rainbowf@mail.ru>
2021-11-17 20:36:20 +0500

    Remove unnecessary functions

--- telegram_bot_post.py
+++ telegram_bot_post.py
@@ -5,16 +5,6 @@ import telegram
 from dotenv import load_dotenv
 
 
-def post_message(bot: telegram.Bot, chat_id: str, text: str) -> None:
-    """Post message in Telegram channel."""
-    bot.send_message(chat_id=chat_id, text=text)
-
-
-def post_image(bot: telegram.Bot, chat_id: str, image: str) -> None:
-    """Post photo in Telegram channel."""
-    bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
-
-
 if __name__ == '__main__':
     load_dotenv()
     token = os.getenv('TELEGRAM_TOKEN')
@@ -22,13 +12,13 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=token)
     chat_id = '@CuriousAstro'
     message_text = 'Добро пожаловать!'
-    post_message(bot, chat_id, message_text)
+    bot.send_message(chat_id=chat_id, text=message_text)
     directory = 'apod_images'
     images_list = os.listdir(directory)
     while True:
         for image_number, image in enumerate(images_list):
             image = f'{directory}/{image}'
-            post_image(bot, chat_id, image)
+            bot.send_photo(chat_id=chat_id, photo=open(image, 'rb'))
             if image_number == len(images_list) - 1:
                 print('All images are published.')
                 exit(0)
